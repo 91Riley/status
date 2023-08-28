96f9b63d77af5dc5139a985359f0cfe81a239b56
91Riley <74956631+91Riley@users.noreply.github.com>
2023-08-28 14:27:44 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://loveyu.cn/" text="月光社区" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
